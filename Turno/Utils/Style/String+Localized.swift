a1df33eb30b79cfac79fb4e323e0663243ef3335
Anan Sadiya <anansadiya@gmail.com>
2020-06-03 20:44:00 +0300

    Fonts were added.

---
+++ Turno/Utils/Style/String+Localized.swift
@@ -0,0 +1,15 @@
+//
+//  String+Localized.swift
+//  Turno
+//
+//  Created by Anan Sadiya on 03/06/2020.
+//  Copyright © 2020 Anan Sadiya. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
