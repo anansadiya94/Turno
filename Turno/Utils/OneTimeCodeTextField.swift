2584f217d3c6fc457697d1337d267cc916ef3282
Anan Sadiya <anan.sadiya@shutterfly.com>
2021-03-12 09:53:02 +0200

    One time code class added.

--- Turno/Utils/OneTimeCodeTextField.swift
+++ Turno/Utils/OneTimeCodeTextField.swift
@@ -6,4 +6,122 @@
 //  Copyright © 2021 Anan Sadiya. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class OneTimeCodeTextField: UITextField {
+    
+    var didEnterLastDigit: ((String) -> Void)?
+    private var isConfigured = false
+    private var digitLabels = [UILabel]()
+    private var defaultCharacter = "-"
+    private var lazytapGestureRecognizer: UITapGestureRecognizer {
+        let tapGestureRecognizer = UITapGestureRecognizer()
+        tapGestureRecognizer.addTarget(self, action: #selector(becomeFirstResponder))
+        return tapGestureRecognizer
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    func configure(with slotCount: Int = 6) {
+        guard isConfigured == false else { return }
+        isConfigured.toggle()
+        
+        configureTextField()
+        
+        let labelsStackView = createLabelsStackView(with: slotCount)
+        addSubview(labelsStackView)
+        
+        addGestureRecognizer(lazytapGestureRecognizer)
+        
+        NSLayoutConstraint.activate([
+            labelsStackView.topAnchor.constraint(equalTo: topAnchor),
+            labelsStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            labelsStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            labelsStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    func becomeFirstResponderFirstLabel() {
+        digitLabels.first?.becomeFirstResponder()
+    }
+    
+    func reset() {
+        self.text = ""
+        digitLabels.forEach({ label in
+            label.text?.removeAll()
+            label.text = defaultCharacter
+        })
+        becomeFirstResponder()
+    }
+    
+    private func configureTextField() {
+        tintColor = .clear
+        textColor = .clear
+        keyboardType = .numberPad
+        textContentType = .oneTimeCode
+        
+        addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        delegate = self
+    }
+    
+    private func createLabelsStackView(with count: Int) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        
+        for _ in 1...count {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.textAlignment = .center
+            label.font = Fonts.Bold20
+            label.backgroundColor = .white
+            label.isUserInteractionEnabled = true
+            label.layer.cornerRadius = 5
+            label.layer.borderWidth = 2
+            label.layer.borderColor = UIColor.black.cgColor
+            label.text = defaultCharacter
+            
+            stackView.addArrangedSubview(label)
+            
+            digitLabels.append(label)
+        }
+        return stackView
+    }
+    
+    @objc private func textDidChange() {
+        guard let text = self.text, text.count <= digitLabels.count else { return }
+        
+        for i in 0 ..< digitLabels.count {
+            let currentLabel = digitLabels[i]
+            
+            if i < text.count {
+                let index = text.index(text.startIndex, offsetBy: i)
+                currentLabel.text = String(text[index])
+            } else {
+                currentLabel.text?.removeAll()
+            }
+        }
+        
+        if text.count == digitLabels.count {
+            didEnterLastDigit?(text)
+        }
+    }
+}
+
+extension OneTimeCodeTextField: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let characterCount = textField.text?.count else { return false }
+        return characterCount < digitLabels.count || string.isEmpty
+    }
+}
