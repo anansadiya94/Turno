a98a38f226e93c90e99fb96473e4da5c58db2e1e
Anan Sadiya <anan.sadiya@shutterfly.com>
2021-03-18 23:03:49 +0200

    PushNotificationManager added

--- Turno/Utils/PushNotificationManager.swift
+++ Turno/Utils/PushNotificationManager.swift
@@ -6,4 +6,86 @@
 //  Copyright © 2021 Anan Sadiya. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseMessaging
+import UserNotifications
+
+class PushNotificationManager: NSObject {
+    
+//    let userID: String
+    let gcmMessageIDKey = "gcm.Message_ID"
+//    init(userID: String) {
+//        self.userID = userID
+//        super.init()
+//    }
+    
+    func registerForPushNotifications() {
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+            Messaging.messaging().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+        }
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+    
+    func updateFirestorePushTokenIfNeeded() {
+        if let token = Messaging.messaging().fcmToken {
+            let usersRef = Firestore.firestore().collection("users_table").document("123456")
+            usersRef.setData(["fcmToken": token], merge: true)
+        }
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+}
+
+// MARK: - MessagingDelegate
+extension PushNotificationManager: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let token = Messaging.messaging().fcmToken {
+            let usersRef = Firestore.firestore().collection("users").document("123456")
+            usersRef.setData(["fcmToken": token], merge: true)
+        }
+    }
+}
+// MARK: - UNUserNotificationCenterDelegate
+@available(iOS 10, *)
+extension PushNotificationManager: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        completionHandler([[.alert, .sound]])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
